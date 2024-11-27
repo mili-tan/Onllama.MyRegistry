510ad239f48afe3b885c20405468ca5cb11c8723
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2024-11-27 20:00:57 +0800

    +CLI

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using System.Runtime.InteropServices;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace Onllama.MyRegistry
 {
@@ -11,6 +13,23 @@ namespace Onllama.MyRegistry
     {
         static void Main(string[] args)
         {
+            var isZh = Thread.CurrentThread.CurrentCulture.Name.Contains("zh");
+            var cmd = new CommandLineApplication
+            {
+                Name = "Onllama.MyRegistry",
+                Description = "Onllama.MyRegistry - Running your own Ollama Registry locally." +
+                              Environment.NewLine +
+                              $"Copyright (c) {DateTime.Now.Year} Milkey Tan. Code released under the MIT License"
+            };
+            cmd.HelpOption("-?|-h|--help");
+
+            var ipOption = cmd.Option<string>("-l|--listen <IPEndPoint>",
+                isZh ? "监听的地址与端口。" : "Set server listening address and port",
+                CommandOptionType.SingleValue);
+            var modelPathOption = cmd.Option<string>("-m|--model <path>",
+                isZh ? "模型文件路径。" : "Set model file path",
+                CommandOptionType.SingleValue);
+            var listen = new IPEndPoint(IPAddress.Any, 80);
             var modelPath = Environment.GetEnvironmentVariable("OLLAMA_MODELS") ??
                             Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ollama",
             "models");
@@ -18,120 +37,127 @@ namespace Onllama.MyRegistry
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && Directory.Exists("/usr/share/ollama/.ollama/models"))
                 modelPath = "/usr/share/ollama/.ollama/models";
 
-            Console.WriteLine("ModelPath:" + modelPath);
-
-            try
+            cmd.OnExecute(() =>
             {
-                var host = new WebHostBuilder()
-                    .UseKestrel()
-                    .UseContentRoot(AppDomain.CurrentDomain.SetupInformation.ApplicationBase)
-                    .ConfigureServices(services =>
-                    {
-                        services.AddRouting();
-                    })
-                    .ConfigureKestrel(options =>
-                    {
-                        options.ListenAnyIP(80,
-                            listenOptions =>
-                            {
-                                //listenOptions.UseHttps();
-                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
-                            });
-                    })
+                if (ipOption.HasValue()) listen = IPEndPoint.Parse(ipOption.Value());
+                if (modelPathOption.HasValue()) modelPath = modelPathOption.Value();
+                Console.WriteLine("ModelPath:" + modelPath);
 
-                    .Configure(app =>
-                    {
-                        app.Use(async (context, next) =>
+                try
+                {
+                    var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(AppDomain.CurrentDomain.SetupInformation.ApplicationBase)
+                        .ConfigureServices(services => { services.AddRouting(); })
+                        .ConfigureKestrel(options =>
                         {
-                            Console.WriteLine(context.Request.Path);
-                            await next.Invoke();
-                        });
-                        app.Map("/v2", svr =>
-                        {
-                            app.UseRouting().UseEndpoints(endpoint =>
-                            {
-                                app.Use(async (context, next) =>
+                            options.Listen(listen,
+                                listenOptions =>
                                 {
-                                    Console.WriteLine(context.Request.Path);
-                                    await next.Invoke();
+                                    //listenOptions.UseHttps();
+                                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
                                 });
+                        })
 
-                                endpoint.Map(
-                                    "/v2/{rope}/{model}/blobs/{hash}",
-                                    async context =>
+                        .Configure(app =>
+                        {
+                            app.Use(async (context, next) =>
+                            {
+                                Console.WriteLine(context.Request.Path);
+                                await next.Invoke();
+                            });
+                            app.Map("/v2", svr =>
+                            {
+                                app.UseRouting().UseEndpoints(endpoint =>
+                                {
+                                    app.Use(async (context, next) =>
                                     {
-                                        foreach (var itemHeader in context.Request.Headers)
-                                            Console.WriteLine(itemHeader.Key + ":" + itemHeader.Value);
+                                        Console.WriteLine(context.Request.Path);
+                                        await next.Invoke();
+                                    });
 
-                                        var hash = context.Request.RouteValues["hash"].ToString().Replace(':', '-');
-                                        var path = Path.Combine(modelPath, "blobs", hash);
-                                        if (File.Exists(path))
+                                    endpoint.Map(
+                                        "/v2/{rope}/{model}/blobs/{hash}",
+                                        async context =>
                                         {
-                                            var fileLength = new FileInfo(path).Length;
+                                            foreach (var itemHeader in context.Request.Headers)
+                                                Console.WriteLine(itemHeader.Key + ":" + itemHeader.Value);
 
-                                            //处理Range请求
-                                            if (context.Request.Headers.ContainsKey("Range"))
+                                            var hash = context.Request.RouteValues["hash"].ToString().Replace(':', '-');
+                                            var path = Path.Combine(modelPath, "blobs", hash);
+                                            if (File.Exists(path))
                                             {
-                                                var rangeHeader = context.Request.Headers["Range"].ToString();
-                                                var range = rangeHeader.Replace("bytes=", "").Split('-');
-                                                var start = long.Parse(range[0]);
-                                                var end = range.Length > 1 ? long.Parse(range[1]) : fileLength - 1;
-                                                var length = end - start + 1;
+                                                var fileLength = new FileInfo(path).Length;
 
-                                                if (start >= fileLength || end >= fileLength)
+                                                //处理Range请求
+                                                if (context.Request.Headers.ContainsKey("Range"))
                                                 {
-                                                    context.Response.StatusCode = StatusCodes.Status416RequestedRangeNotSatisfiable;
-                                                    return;
+                                                    var rangeHeader = context.Request.Headers["Range"].ToString();
+                                                    var range = rangeHeader.Replace("bytes=", "").Split('-');
+                                                    var start = long.Parse(range[0]);
+                                                    var end = range.Length > 1 ? long.Parse(range[1]) : fileLength - 1;
+                                                    var length = end - start + 1;
+
+                                                    if (start >= fileLength || end >= fileLength)
+                                                    {
+                                                        context.Response.StatusCode =
+                                                            StatusCodes.Status416RequestedRangeNotSatisfiable;
+                                                        return;
+                                                    }
+
+                                                    context.Response.Headers.Location = context.Request.Path.ToString();
+                                                    context.Response.StatusCode = StatusCodes.Status206PartialContent;
+                                                    context.Response.ContentType = "application/octet-stream";
+                                                    context.Response.Headers.Add("Content-Range",
+                                                        $"bytes {start}-{end}/{fileLength}");
+                                                    context.Response.ContentLength = length;
+
+                                                    await context.Response.SendFileAsync(path, start, length);
                                                 }
+                                                else
+                                                {
+                                                    context.Response.Headers.Location = context.Request.Path.ToString();
+                                                    context.Response.ContentLength = fileLength;
+                                                    context.Response.StatusCode = StatusCodes.Status200OK;
+                                                    context.Response.ContentType = "application/octet-stream";
+                                                    context.Response.Headers.Add("Content-Disposition",
+                                                        $"attachment; filename={hash}");
 
-                                                context.Response.Headers.Location = context.Request.Path.ToString();
-                                                context.Response.StatusCode = StatusCodes.Status206PartialContent;
-                                                context.Response.ContentType = "application/octet-stream";
-                                                context.Response.Headers.Add("Content-Range", $"bytes {start}-{end}/{fileLength}");
-                                                context.Response.ContentLength = length;
-
-                                                await context.Response.SendFileAsync(path,start, length);
+                                                    await context.Response.SendFileAsync(path);
+                                                }
                                             }
                                             else
-                                            {
-                                                context.Response.Headers.Location = context.Request.Path.ToString();
-                                                context.Response.ContentLength = fileLength;
-                                                context.Response.StatusCode = StatusCodes.Status200OK;
-                                                context.Response.ContentType = "application/octet-stream";
-                                                context.Response.Headers.Add("Content-Disposition", $"attachment; filename={hash}");
+                                                context.Response.StatusCode = 404;
+                                        });
 
+                                    endpoint.Map(
+                                        "/v2/{rope}/{model}/manifests/{tag}",
+                                        async context =>
+                                        {
+                                            var rope = context.Request.RouteValues["rope"].ToString();
+                                            var model = context.Request.RouteValues["model"].ToString();
+                                            var tag = context.Request.RouteValues["tag"].ToString();
+                                            var path = Path.Combine(modelPath, "manifests", "registry.ollama.ai", rope,
+                                                model, tag);
+                                            Console.WriteLine(path);
+                                            if (File.Exists(path))
                                                 await context.Response.SendFileAsync(path);
-                                            }
-                                        }
-                                        else
-                                            context.Response.StatusCode = 404;
-                                    });
-
-                                endpoint.Map(
-                                    "/v2/{rope}/{model}/manifests/{tag}",
-                                    async context =>
-                                    {
-                                        var rope = context.Request.RouteValues["rope"].ToString();
-                                        var model = context.Request.RouteValues["model"].ToString();
-                                        var tag = context.Request.RouteValues["tag"].ToString();
-                                        var path = Path.Combine(modelPath, "manifests", "registry.ollama.ai", rope,
-                                            model, tag);
-                                        Console.WriteLine(path);
-                                        if (File.Exists(path))
-                                            await context.Response.SendFileAsync(path);
-                                        else
-                                            context.Response.StatusCode = 404;
-                                    });
+                                            else
+                                                context.Response.StatusCode = 404;
+                                        });
+                                });
                             });
-                        });
-                    }).Build();
+                        }).Build();
 
-                host.Run();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+                    host.Run();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            });
+
+            cmd.Execute(args);
         }
     }
 }
